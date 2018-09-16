15debf92549f05059f3276c1efecb608a895aa4d
Jackson Zou <jzou00@stuy.edu>
2018-09-16 17:29:13 -0400

    Changed to print name and message; works

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello my name is Jackson Zou, I am very tired");
     }
 }
