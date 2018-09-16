fc609a8fcd0839c9954e5abfcd6bc51bd0a106f2
Jackson Zou <jzou00@stuy.edu>
2018-09-16 17:27:20 -0400

    Fixed bug and successful print of "Hello World"

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-  public static void main(String[] args) {
-    System.out.printIn("Hello world");
-  }
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
 }
